804ed94d13d1b12cf17fcf9a3011edbd378c784b
CheBar1 <chebarclay@live.com.au>
2022-09-18 16:57:38 +0930

    Updated variables

--- script.js
+++ script.js
@@ -3,6 +3,33 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+ 
+
+function generatePassword() {
+
+  var userResponse = window.prompt("How many characters would you like your password to contain?")
+  var passwordLength = parseInt(userResponse)
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password length must be between 8 and 128")
+    return
+  }
+
+  var lowercase = confirm("Click OK to confirm including lowercase characters.")
+  var uppercase = confirm("Click OK to confirm including uppercase characters.")
+  var numeric = confirm("Click OK to confirm including numeric characters.")
+  var special = confirm("Click OK to confirm including special characters.")
+  
+
+//1. Promt the user for the password criteria
+//    a. Password length 8 < 128
+//    b. lowercase, uppercase, numbers, special characters
+//2. Validate the input re character length  & character selected
+//3. Generate password based on criteria
+
+//4. Display password to the page
+  return "Generated password will go here!"
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +42,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//How many characters would you like your password to contain?
+//	- force user to select length between 8 and 128 characters, validate, if not meeting it should throw and error
+
+//  Click OK to confirm including lowercase characters.
+//  Click OK to confirm including uppercase characters.
+//  Click OK to confirm including numeric characters.
+//  Click OK to confirm including special characters.
