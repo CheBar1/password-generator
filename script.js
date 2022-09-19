ca718088969ef9ceb3e31fb09274a00e3aa02240
CheBar1 <chebarclay@live.com.au>
2022-09-19 11:32:35 +0930

    updated javascript  pseudo code

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-// Assignment code here
-
-
 // Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
- 
+
+// Array of options for computer to pick from
+
 
 function generatePassword() {
 
-  var userResponse = window.prompt("How many characters would you like your password to contain?")
-  var passwordLength = parseInt(userResponse)
+  // Promt the user for the password criteria
+  var userInput = window.prompt("How many characters would you like your password to contain?");
+  var passwordLength = parseInt(userInput);
 
+  // Validate the user selects a number
+  if(isNaN(passwordLength)) {
+    window.alert("That is not a number!");
+    return;
+  } else {
+    window.alert("That is a valid number!");
+  }
+  
+  // Validate the user selects a number between 8 and 128 characters long
   if (passwordLength < 8 || passwordLength > 128) {
-    window.alert("Password length must be between 8 and 128")
-    return
+    window.alert("Password length must be between 8 and 128");
+    return;
   }
 
-  var lowercase = confirm("Click OK to confirm including lowercase characters.")
-  var uppercase = confirm("Click OK to confirm including uppercase characters.")
-  var numeric = confirm("Click OK to confirm including numeric characters.")
-  var special = confirm("Click OK to confirm including special characters.")
+  var lowercase = confirm("Click OK to confirm including lowercase characters.");
+  var uppercase = confirm("Click OK to confirm including uppercase characters.");
+  var numeric = confirm("Click OK to confirm including numeric characters.");
+  var special = confirm("Click OK to confirm including special characters.");
+  
   
 
-//1. Promt the user for the password criteria
-//    a. Password length 8 < 128
-//    b. lowercase, uppercase, numbers, special characters
-//2. Validate the input re character length  & character selected
+ 
+
+ 
 //3. Generate password based on criteria
 
 //4. Display password to the page
-  return "Generated password will go here!"
+  return "Generated password will go here!";
 }
 
 // Write password to the #password input
@@ -42,11 +52,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-//How many characters would you like your password to contain?
-//	- force user to select length between 8 and 128 characters, validate, if not meeting it should throw and error
-
-//  Click OK to confirm including lowercase characters.
-//  Click OK to confirm including uppercase characters.
-//  Click OK to confirm including numeric characters.
-//  Click OK to confirm including special characters.
